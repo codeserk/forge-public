5a5c93c0749c00008137fbd948512778fe0669d6
Jose Camara <josec89@gmail.com>
2026-03-22 14:51:11 +0100

    🔧 add GitHub Pages deploy action

--- typescript/apps/docs/astro.config.mjs
+++ typescript/apps/docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://forge.codeserk.es',
   integrations: [
     starlight({
       title: 'Forge',
